7cab34a106dcc9113029997676638c2e7f65935a
kotik322322 <kotan23111995@gmail.com>
2024-05-04 01:06:01 +0300

    started mobile version about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,7 +9,6 @@ import { useAnimation } from 'framer-motion';
 import Skills from '../components/Skills';
 import Experience from '../components/Experience';
 
-
 const AboutPage = () => {
   return (
     <div>
@@ -21,8 +20,11 @@ const AboutPage = () => {
         <Layout className="pt-16">
           <AnimatedText text="Passion Fuel Purpose" className="mb-16" />
           <div className="grid w-full grid-cols-8 gap-16">
-            <div className="col-span-3 flex flex-col items-start justify-start">
-              <h2 className="mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75">Biography</h2>
+
+            <div className="col-span-4 lg:col-span-3 flex flex-col items-start justify-start order-2">
+              <h2 className="mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75">
+                Biography
+              </h2>
 
               <p className="font-medium">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, neque tenetur.
@@ -49,44 +51,52 @@ const AboutPage = () => {
               </p>
             </div>
 
-            <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light">
+            <div className="col-span-8 lg:col-span-3  relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light order-1 ">
               <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light" />
               <Image
                 src={profilePic}
                 alt="Kostiantyn Marfin"
                 className="w-full h-auto rounded-2xl"
                 priority
-                sizes='(max-width: 768px) 100vw,
+                sizes="(max-width: 768px) 100vw,
                 (max-width:1200px) 50vw,
                 50vw
-                '
+                "
               />
             </div>
 
-            <div className='col-span-2 flex flex-col items-end justify-between'>
-
-              <div className='flex flex-col items-end justify-center'>
-                <span className='inline-block text-7xl font-bold'><AnimatedNumbers value={50}/>+</span>
-                <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light'>Tratatam tratatam</h2>
+            <div className="col-span-8 lg:col-span-2 flex flex-row lg:flex-col items-end justify-between order-3">
+              <div className="flex flex-col items-end justify-center">
+                <span className="inline-block text-7xl font-bold">
+                  <AnimatedNumbers value={50} />+
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light">
+                  Tratatam tratatam
+                </h2>
               </div>
 
-              <div className='flex flex-col items-end justify-center'>
-                <span className='inline-block text-7xl font-bold'><AnimatedNumbers value={30}/>+</span>
-                <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light'>Tratatam tratatam</h2>
+              <div className="flex flex-col items-end justify-center">
+                <span className="inline-block text-7xl font-bold">
+                  <AnimatedNumbers value={30} />+
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light">
+                  Tratatam tratatam
+                </h2>
               </div>
 
-              <div className='flex flex-col items-end justify-center'>
-                <span className='inline-block text-7xl font-bold'><AnimatedNumbers value={40}/>+</span>
-                <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light'>Tratatam tratatam</h2>
+              <div className="flex flex-col items-end justify-center">
+                <span className="inline-block text-7xl font-bold">
+                  <AnimatedNumbers value={40} />+
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light">
+                  Tratatam tratatam
+                </h2>
               </div>
-
             </div>
-
-
           </div>
 
-          <Skills/>
-          <Experience/>
+          <Skills />
+          <Experience />
         </Layout>
       </main>
     </div>
