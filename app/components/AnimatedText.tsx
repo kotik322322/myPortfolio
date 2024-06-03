580c410a113739a89574d4c6d447e2500e8e0dc2
kotik322322 <kotan23111995@gmail.com>
2024-06-03 20:17:14 +0300

    fixed styles maine page

--- app/components/AnimatedText.tsx
+++ app/components/AnimatedText.tsx
@@ -36,14 +36,14 @@ interface AnimatedTextProps {
 
 const AnimatedText = ({ text, className }: AnimatedTextProps) => {
   return (
-    <div className="w-full mx-auto py-2 flex items-center justify-center text-center overflow-hidden sm:py-0">
+    <div className="w-full flex items-center justify-center text-center overflow-hidden sm:py-0">
       <motion.h1
         variants={quote}
         initial="initial"
         animate="animate"
         className={`inline-block w-full text-dark font-bold capitalize ${className} dark:text-light`}>
         {text.split(' ').map((word: string, index: number) => (
-          <motion.span variants={singleWord} key={word + '-' + index} className="inline-block text-4xl">
+          <motion.span variants={singleWord} key={word + '-' + index} className="inline-block">
             {word}&nbsp;
           </motion.span>
         ))}
--- app/page.tsx
+++ app/page.tsx
@@ -1,59 +1,56 @@
 import Image from 'next/image';
 import Layout from './components/Layout';
-import profileImg from '../public/images/devpic1.jpg';
+import profileImg from '../public/profile.jpg';
 import AnimatedText from './components/AnimatedText';
 import Link from 'next/link';
 import { LinkArrow } from './components/Icons';
-import HireMe from './components/HireMe';
-
 
 export default function Home() {
   return (
     <main className="flex items-center text-dark w-full min-h-screen dark:text-light ">
       <Layout>
-        <div className="flex flex-col items-center justify-between w-full lg:flex-row">
-          <div className="w-full md:hidden lg:inline-block lg:w-1/2 ">
-            <Image
-              src={profileImg}
-              alt=""
-              className="w-full h-auto  inline-block"
-              priority
-              sizes="(max-width: 768px) 100vw,
-                (max-width: 1200px) 50vw,
-                50vw "
-            />
+        <div className="flex flex-col items-center justify-between gap-y-4 my-4 w-full lg:flex-row lg:gap-x-20">
+          {/* Image */}
+          <div className="w-full relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-3 lg:p-8 dark:bg-dark dark:border-light ">
+            <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light" />
+            <Image src={profileImg} alt="Profile Image" className="w-full h-auto rounded-2xl" />
           </div>
+          {/* Image */}
 
+          {/* =====Text===== */}
           <div className="w-full flex flex-col items-center justify-center text-center lg:text-left lg:w-1/2">
             <AnimatedText
-              text="Turning Vision Into Reality with Code"
-              className="text-center text-3xl sm:text-4xl md:text-5xl lg:text-6xl lg:text-left"
+              text="Transforming Ideas into Innovative Solutions with Code"
+              className="text-2xl md:text-5xl lg:text-6xl "
             />
-            <p className="my-4 font-medium text-xs md:text-sm xl:text-base ">
-              As a skilled Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus sequi
-              iure ipsam optio fugit?
+            <p className="text-left font-medium text-sm xl:text-base ">
+              Hello! My name is Konstantin, and I am a frontend developer with a deep passion for
+              creating modern and high-quality web applications. My core tech stack includes React,
+              Next.js, Redux Toolkit, and TailwindCSS. Throughout my career, I have accumulated
+              significant experience in developing dynamic and responsive user interfaces, ensuring
+              an excellent user experience and optimal application performance. I strive to apply
+              best development practices and continuously update my knowledge of new technologies
+              and tools
             </p>
-
-            <div className="w-full flex items-center justify-center lg:justify-start mt-2 ">
-              <Link
-                href="/resume.pdf"
-                target="_blank"
-                className="flex items-center bg-dark text-light p-2 px-4 rounded-lg text-base font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light transition-all duration-150  md:text-lg md:py-2.5 md:px-6"
-                download={true}>
-                Resume
-                <LinkArrow className="w-6 ml-1" />
-              </Link>
-              <Link
-                href="kotan23111995@gmail.com"
-                className="ml-4 p-2 px-4 text-lg font-medium capitalize text-dark underline dark:text-light md:text-lg md:py-2.5 md:px-6">
-                Contact
-              </Link>
-            </div>
+          </div>
+          {/* =====Text===== */}
+          <div className="w-full flex items-center justify-center lg:justify-start ">
+            <Link
+              href="/resume.pdf"
+              target="_blank"
+              className="flex items-center bg-dark text-light p-2 px-4 rounded-lg text-base font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light transition-all duration-150  md:text-lg md:py-2.5 md:px-6"
+              download={true}>
+              Resume
+              <LinkArrow className="w-6 ml-1" />
+            </Link>
+            <Link
+              href="kotan23111995@gmail.com"
+              className="ml-4 p-2 px-4 text-lg font-medium capitalize text-dark underline dark:text-light md:text-lg md:py-2.5 md:px-6">
+              Contact
+            </Link>
           </div>
         </div>
       </Layout>
-
-      {/* <HireMe /> */}
     </main>
   );
 }
